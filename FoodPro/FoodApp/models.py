1069839259894780e9d59a5d8c65eeea77293eb6
amitsaini95 <amitsaini201295@gmail.com>
2026-01-31 14:25:19 +0800

    create user model

--- FoodPro/FoodApp/models.py
+++ FoodPro/FoodApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+GENDER=(
+    ('Male','Male'), 
+    ('Female','Female'),
+    ('Other','Other'),
+
+)
+class User(AbstractUser):
+    age=models.PositiveIntegerField(null=True)
+    gender=models.CharField(choices=GENDER,max_length=10)
+    phoneNo=models.IntegerField(null=True)
+    address=models.TextField(null=True)
+
+    def __str__(self):
+        return self.username

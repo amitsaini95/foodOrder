294e24f0d60ec8fb6ff523264eaa8e1b74f89b9a
amitsaini95 <amitsaini201295@gmail.com>
2026-02-02 08:40:38 +0800

    create food model and category mode

--- FoodPro/FoodApp/models.py
+++ FoodPro/FoodApp/models.py
@@ -7,11 +7,32 @@ GENDER=(
     ('Other','Other'),
 
 )
+class Category(models.Model):
+    name = models.CharField(max_length=200,null = False, blank = False)
+    image = models.CharField(max_length=3000,null = True, blank = True)
+    description = models.TextField(max_length=1000,null = False, blank = False)
+    
+    def __str__(self):
+        return self.name
 class User(AbstractUser):
     age=models.PositiveIntegerField(null=True)
     gender=models.CharField(choices=GENDER,max_length=10)
     phoneNo=models.IntegerField(null=True)
     address=models.TextField(null=True)
-
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS=['email']
     def __str__(self):
         return self.username
+class Food(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=400,null = False, blank = False)
+    item_description = models.TextField(max_length=1000,null = False, blank = False)
+    price = models.FloatField(null = False, blank = False)
+    offer_price = models.FloatField(null = False, blank = False)
+    item_image = models.CharField(max_length=3000,null = True, blank = True)
+    quantity = models.IntegerField(null = False, blank = False)
+    underrated_item = models.BooleanField(default=False,help_text="0-show, 1-hidden")
+    new_added_item = models.BooleanField(default=False,help_text="0-show, 1-hidden")
+
+    def __str__(self):
+        return f'{self.name}'
